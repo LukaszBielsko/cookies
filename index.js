56810a9bc98ee40b1ee48931cf1448f4b5cf4170
Lukasz Walczak <lukeabove@gmail.com>
2019-12-17 09:12:53 +0000

    first cookie succesfully added

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-const session = require("express-session");
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => {
+  res.cookie("cookie", "fist cookie ever");
+  res.send("Hello You and Me");
+});
 
 app.listen(port, () => console.log("App listening on port " + port));
+
+/* The order in which you use middleware in Express matters: middleware declared earlier will get called first, and if it can handle a request, any middleware declared later will not get called. */
+
+/*
+ res.cookie("cookie", "fist cookie ever");
+ above will produce the below in the console
+ document.cookie
+"cookie=fist%20cookie%20ever" */
