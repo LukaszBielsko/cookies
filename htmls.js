965f16f5e5a0e6b96814bb63c25e198df9587c79
Lukasz Walczak <lukeabove@gmail.com>
2019-12-19 10:29:59 +0100

    cosmetic changes to html

---
+++ htmls.js
@@ -0,0 +1,6 @@
+module.exports = {
+  knownUser:
+    " <h1>You are no stranger to me! </h1> <h1>You have been here before.</h1>",
+  stranger:
+    " <h1>Hello, Stranger! </h1> <h1>I see you for the first time here.</h1>"
+};
--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-const ex = '<div> <p>Hi, Luk! </p> <a href="/hiya">hiya</a></div>';
-const stranger = '<div> <p>Hi, Stranger! </p> <a href="/hiya">hiya</a></div>';
+const html = require("./htmls");
 
 const logger = (req, res, next) => {
   console.log(req.originalUrl);
@@ -13,32 +11,28 @@ const logger = (req, res, next) => {
 app.use(logger);
 
 app.get("/", (req, res) => {
-  console.log("get: root");
-  res.cookie("cookie", "1");
+  res.cookie("cookie", "1"); // set a cookie with a name of 'cookie' and value of '1' cookie=1
   if (req.headers.cookie) {
     let cookie = req.headers.cookie.split("=");
     const cookieValue = cookie[1];
-    res.send(ex);
+    res.send(html.knownUser);
   }
-  res.send(stranger);
-});
-
-app.get("/hiya", (req, res) => {
-  console.log("get: hiya");
-  res.send("hiya");
+  res.send(html.stranger);
 });
 
 app.listen(port, () => console.log("App listening on port " + port));
 
 /* 
-  The order in which you use middleware in Express matters: middleware declared earlier will get called first, and if it can handle a request, any middleware declared later will not get called. 
+  The order in which you use middleware in Express matters: middleware 
+  declared earlier will get called first, and if it can handle a request, 
+  any middleware declared later will not get called. 
 */
 
 /*
- res.cookie("cookie", "fist cookie ever");
- above will produce the below in the console
- document.cookie
-"cookie=fist%20cookie%20ever" 
+  res.cookie("cookie", "fist cookie ever");
+  above will produce the below in the console
+  document.cookie
+  "cookie=fist%20cookie%20ever" 
 */
 
 /* 
